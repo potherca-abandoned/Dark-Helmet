241205f7353513f89649008b430a88ce8480e2cd
Ben Peachey <potherca@hotmail.com>
2011-03-13 21:49:21 +0100

    Added GPL to index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
+/**
+ * @author:	 Potherca <potherca@gmail.com>
+ * @package: DarkHelmet
+ * @link:	 https://github.com/potherca/Dark-Helmet/
+ * @license: Copyright 2011, Potherca
+ *
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/gpl.html>.
+ * 
+ */
 namespace DarkHelmet
 {
 	use DarkHelmet\Core\Exception;
