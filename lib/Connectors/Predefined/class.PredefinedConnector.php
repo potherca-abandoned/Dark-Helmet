4cc949bc4b3e6fb4a0c4da85d18fb6baecd4a994
ben <ben@ben-PC-Ubuntu.(none)>
2011-03-14 10:59:33 +0100

    Made the PredefinedConnector check files existence before trying to open it, hence not causing an error anymore.

--- lib/Connectors/Predefined/class.PredefinedConnector.php
+++ lib/Connectors/Predefined/class.PredefinedConnector.php
@@ -17,21 +17,28 @@ namespace DarkHelmet\Connectors\Predefined
 			$aParsedTags = $p_aTags;
 			$aPrefixes = $p_oContext->get('aPrefix');
 			$sFilePath   = $this->getParam('TagFilePath');
-			$aTags       = json_decode(file_get_contents($sFilePath),true);
-			foreach($aTags as $t_sCategory => $t_aTags) {
-				foreach($t_aTags as $t_sTag) {
-					$sTag = Tags::stripspaces($t_sTag);
-					$sCaption = str_replace('_', ' ', $sTag);
 
-					if(isset ($aPrefixes[$t_sCategory])){
-						$aParsedTags[] = array(
-							  'caption' => $aPrefixes[$t_sCategory] . $sCaption
-							, 'value' => $aPrefixes[$t_sCategory] . $sTag
-							, 'addClass' => $t_sCategory
-						);
-					}
+			if(!is_readable($sFilePath)){
+				$aParsedTags[] = array(Tags::tagArray($p_oContext, '__ERROR__', 'Could not read predefined tags file.', ''));
+			}
+			else
+			{
+				$aTags = json_decode(file_get_contents($sFilePath),true);
+				foreach($aTags as $t_sCategory => $t_aTags) {
+					foreach($t_aTags as $t_sTag) {
+						$sTag = Tags::stripspaces($t_sTag);
+						$sCaption = str_replace('_', ' ', $sTag);
+
+						if(isset ($aPrefixes[$t_sCategory])){
+							$aParsedTags[] = array(
+								  'caption' => $aPrefixes[$t_sCategory] . $sCaption
+								, 'value' => $aPrefixes[$t_sCategory] . $sTag
+								, 'addClass' => $t_sCategory
+							);
+						}
+					}#foreach
 				}#foreach
-			}#foreach
+			}#if
 
 			return $aParsedTags;
 		}
