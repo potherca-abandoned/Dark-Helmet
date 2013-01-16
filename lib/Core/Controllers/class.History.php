19c7bfb4e6358f7f1c05c33bd9d87ee19d37d1c9
Ben Peachey <potherca@gmail.com>
2013-01-16 16:34:39 +0100

    Adds experimental History Listing.
    This is a first rough draft to get a list of logged dates and a tally of how much time was logged per day and week.
    It still needs to but put in the right location and refined.
    Accessable under the `/history/` URL (without any parameters).

--- lib/Core/Controllers/class.History.php
+++ lib/Core/Controllers/class.History.php
@@ -40,13 +40,81 @@ namespace DarkHelmet\Core\Controllers
 
 //			return $this->buildTemplateOutputFromContext($this->getContext());
 ////////////////////////////////////////////////////////////////////////////////
-			$aHistory = array();
+            //@TODO: Show the History Listing
+            /*
+                To properly implement this we need to add another required
+                method to the historyConnector interface that requires triggered
+                HistoryProviders to provide a list we can then turn into:
+
+                    <li><a href="history/$tag"></a></li>
+            */
+            $aHistory = array();
 
 			if(empty($this->m_sDate)){
-				// Show the History Listing
-                // To implement this we need to add another required method to the historyConnector interface that
-                // requires triggered HistoryProviders to provide a list we can then turn into <li><a href="history/$tag"></a></li>s
-				die('@TODO: Show History Listing...');
+                $oContext->set('bShowForm', false);
+
+				$aDayTotals = array();
+				$aWeekTotals = array();
+
+                $iCurrentWeek = 0;
+                $iWeekTally = 0;
+				foreach($this->getSettings()->getConnectors() as $t_oConnector){
+					if($t_oConnector instanceof \DarkHelmet\Connectors\Local\LocalConnector){
+						/** @var $t_oConnector \DarkHelmet\Connectors\Local\LocalConnector */
+						$aHistoryList = $t_oConnector->getHistoryList();
+						$aKeys = array_keys($aHistoryList);
+						$oContext->set('keys', $aKeys);
+
+
+						foreach($aHistoryList as $t_sDate => $t_sFilePath){
+							/** @var $oTimeLog \DarkHelmet\Core\TimeLog */
+							$oTimeLog = $t_oConnector->createTimeLogFromDateString($t_sDate, $oContext);
+
+							$t_aTotals = $oTimeLog->calculateTotals();
+							$sTotalSeconds = null;
+							if(isset($t_aTotals['ALL']['__TOTAL__']))
+							{
+								/** @var $oTotal DateTime */
+								$oTotal = $t_aTotals['ALL']['__TOTAL__'];
+								$sTotalSeconds = $oTotal->format('U');
+							}#if
+							$aDayTotals[$t_sDate ] = $sTotalSeconds;
+						}#foreach
+
+						krsort($aDayTotals); // Newest first
+						foreach($aDayTotals as $t_sDay => $t_sDayTotalSeconds)
+						{
+                            $sTimestamp = strtotime($t_sDay);
+                            echo sprintf(
+                                '<li>
+                                    <a href="history/%1$s"> %2$s </a>
+                                    <span class="day-total">%3$s</span>
+                                </li>'
+                                , $t_sDay
+                                , date('D j F Y', $sTimestamp)
+                                , gmdate("H:i:s", $t_sDayTotalSeconds)
+                            );
+
+                            $iWeek = date('W', $sTimestamp);
+                            if($iWeek !== $iCurrentWeek) {
+                                $iWeekTally += $t_sDayTotalSeconds;
+                                echo sprintf(
+                                      '</ul><p class="week-total">Week Total: <span>%2$s</span></p><hr/>
+                                       <p class="week">Week %1$s/%3$s: <span>%2$s</span></span></p><ul>
+                                      '
+                                    , $iWeek
+                                    , sprintf("%02d:%02d:%02d", floor($iWeekTally/3600), ($iWeekTally/60)%60, $iWeekTally%60)
+                                    , date('Y', $sTimestamp)
+
+                                );
+                                $iCurrentWeek = $iWeek;
+                                $iWeekTally = 0;
+                            } else {
+                                $iWeekTally += $t_sDayTotalSeconds;
+                            }
+						}
+					}#if
+				}#foreach
 			}
 			else if($this->m_sDate !== $oContext->get('sToday')){
 				foreach($this->getSettings()->getConnectors() as $t_oConnector){
